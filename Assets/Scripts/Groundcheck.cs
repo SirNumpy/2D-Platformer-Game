f7f07679032220bc7636cf75bdbb86f4194f2d20
Prabhat Madan <1228prabhat@gmail.com>
2022-01-26 17:41:15 +0530

    clumsy code change to better one
    better code

---
+++ Assets/Scripts/Groundcheck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Groundcheck : MonoBehaviour
+{
+    [SerializeField] public bool isGrounded;
+    
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+            Debug.Log("collided with the ground");
+        }
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private Vector3 localTransform;
-    private float horizontalInput;
     private Rigidbody2D rigidbody2d;
-    void Start()
+    public float speed;
+    private Vector3 position;
+    public Groundcheck groundcheck;
+    public BoxCollider2D crouchcollider;
+    public CapsuleCollider2D idleCollider;
+    //private bool iscrouch=false;
+    private bool isJump = false;
+    public float jumpforce;
+    private void Awake()
     {
         animator = GetComponent<Animator>();
-        localTransform = transform.localScale;
         rigidbody2d = GetComponent<Rigidbody2D>();
-        
+        crouchcollider = GetComponent<BoxCollider2D>();
+        idleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Start()
+    {
+        localTransform = transform.localScale;
+        position = transform.position;
+        crouchcollider.enabled = false;
+
     }
 
     
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        //horizontal movement and flipping
+        float horizontalInput = Input.GetAxis("Horizontal");
         if (horizontalInput != 0)
         {
             
@@ -35,14 +53,57 @@ public class PlayerController : MonoBehaviour
             }
             animator.SetFloat("speed", 0.8f);
             transform.localScale = localTransform;
-            rigidbody2d.AddForce(new Vector2(10.0f, 0) * 50.0f *horizontalInput* Time.deltaTime, ForceMode2D.Force);
+            position = new Vector3( position.x+speed * Time.deltaTime*horizontalInput,transform.position.y,0);
+            transform.position = position;
 
         }
         else
         {
             animator.SetFloat("speed", 0.0f);
         }
+        //vertical movement
+        if (Input.GetKeyDown(KeyCode.Space)&& groundcheck.isGrounded==true)
+        {
+            animator.SetBool("isJump", true);
+            isJump = true;
+            
+        }
+        
+
+        
+
+
+
+
+        //crouch working
+        if (Input.GetKey(KeyCode.C))
+        {
+            animator.SetBool("isCrouch", true);
+            crouchcollider.enabled = true;
+            idleCollider.enabled = false;
+        }
+        else
+        {
+            animator.SetBool("isCrouch", false);
+            crouchcollider.enabled = false;
+            idleCollider.enabled = true;
+
+        }
+       
         
         
     }
+    private void FixedUpdate()
+    {
+        if (isJump == true)
+        {
+            rigidbody2d.AddForce(Vector2.up * jumpforce * Time.deltaTime, ForceMode2D.Impulse);
+            groundcheck.isGrounded = false;
+            isJump = false;
+            animator.SetBool("isJump", false);
+
+        }
+    }
+
+
 }
